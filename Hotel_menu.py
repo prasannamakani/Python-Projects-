9c9072feaeed910e9f9abe06d0739d4ca93282ee
prasannamakani <prasannamakani16@mail.com>
2024-10-24 15:31:10 +0530

    Hotel Menu mini python project

---
+++ Hotel_menu.py
@@ -0,0 +1,27 @@
+menu={
+    "pizza":99,
+   "manchuria":60,
+    "burger":40,
+    "past":75,
+    "coke":30,
+    "noodles":60
+}
+bill=0
+item_1=input("Enter the item what do you want: ")
+if item_1 in menu:
+    bill+=menu[item_1]
+    print(f"Your  item {item_1}  has been added to your order.")
+else:
+    print(f"Your ordered item {item_1}  not avaliable right now.")
+
+
+another= input("Do you want another item (yes/no): ")
+if another =="yes":
+    item_2=input("Enter the item what do you want: ")
+    if item_2 in menu:
+         bill+=menu[item_2]
+         print(f"Your  another item {item_2}  has been added to your order.")
+    else: print(f"Your ordered item {item_2}  not avaliable right now.")
+print(f"Your bill is {bill} rupees only.")    
+
+       
